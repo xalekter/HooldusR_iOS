088420508f87bd8ac84e0ba68a84ca8d1b4ebd0a
Oleksandr <f>
2026-09-18 09:46:30 +0300

    Initial Commit

---
+++ HooldusR_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HooldusR_iOS
+//
+//  Created by Oleksandr Borysenko on 18.09.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HooldusR_iOS/HooldusR_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HooldusR_iOSApp.swift
+//  HooldusR_iOS
+//
+//  Created by Oleksandr Borysenko on 18.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct HooldusR_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
